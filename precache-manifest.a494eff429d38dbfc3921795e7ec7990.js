c5574e1e489e321c4ce72e93072b3e12211e15a2
Vitalii Kostenko <apelsin@i.ua>
2024-09-04 17:08:31 +0200

    Updates

---
+++ precache-manifest.a494eff429d38dbfc3921795e7ec7990.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f0cf4ceca33bfcb2ac0",
+    "url": "./static/css/main.1f1d5e01.chunk.css"
+  },
+  {
+    "revision": "4f0cf4ceca33bfcb2ac0",
+    "url": "./static/js/main.72365d82.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "d9a116fccb5d4d60bfa9",
+    "url": "./static/js/2.e5c7248e.chunk.js"
+  },
+  {
+    "revision": "5f5205fd6a268ace29c0988933137cc3",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/StarWarsDeploy/build/precache-manifest.885c195cbb1e685dcf4e9b1a49406863.js"
+  "./precache-manifest.a494eff429d38dbfc3921795e7ec7990.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/StarWarsDeploy/build/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
