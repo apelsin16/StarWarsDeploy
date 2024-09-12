284d6bc761c7e6e69647ceb9a31f1da8bb9d118e
Vitalii Kostenko <apelsin@i.ua>
2024-09-12 08:20:39 +0200

    ready

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/app';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename='/StarWarsDeploy' >
         <App />
     </BrowserRouter>,
     document.getElementById('root'));
